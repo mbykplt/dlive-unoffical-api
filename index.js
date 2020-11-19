5f08de54f6178424952fe3475b207f720b7f84c6
elywashere <36236673+mbykplt@users.noreply.github.com>
2020-11-19 16:28:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,4 +36,4 @@ app.get("/live",function(req,result){
     }
   })
 })
-app.listen(3000 || process.env.PORT); //3000 portunda dinleniyor proje.
+app.listen(3000 || process.env.TOKEN ,()=>{console.log("API Hazır, 3000 portunda aktif olarak çalışıyor...")})
